c9e3f24747a1045f1d7a56dd88bd50db3efc195f
Marcel <mk@freeshell.de>
2021-07-26 17:09:27 +0200

    Update

--- locale/dsbxinput_de.ts
+++ locale/dsbxinput_de.ts
@@ -1,153 +1,225 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="de">
+<!DOCTYPE TS><TS version="2.0" language="de" sourcelanguage="">
 <context>
     <name>DevsWidget</name>
     <message>
+        <location filename="../dsbxinput.in" line="649"/>
         <source>Device:</source>
-        <translation type="vanished">Gerät:</translation>
+        <translation>Gerät:</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="650"/>
         <source>Enable device</source>
-        <translation type="vanished">Gerät aktivieren</translation>
+        <translation>Gerät aktivieren</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="721"/>
         <source>Handedness</source>
-        <translation type="vanished">Händigkeit</translation>
+        <translation>Händigkeit</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="723"/>
         <source>Left handed</source>
-        <translation type="vanished">Linkshändig</translation>
+        <translation>Linkshändig</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="749"/>
         <source>Left</source>
-        <translation type="vanished">Links</translation>
+        <translation>Links</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="750"/>
         <source>Middle</source>
-        <translation type="vanished">Mitte</translation>
+        <translation>Mitte</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="751"/>
         <source>Right</source>
-        <translation type="vanished">Rechts</translation>
+        <translation>Rechts</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="802"/>
         <source>Natural scrolling</source>
-        <translation type="vanished">Natürliches Scrollen</translation>
+        <translation>Natürliches Scrollen</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="811"/>
         <source>Horizontal scrolling</source>
-        <translation type="vanished">Horizontales Scrollen</translation>
+        <translation>Horizontales Scrollen</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="863"/>
         <source>Enable tapping</source>
-        <translation type="vanished">Tapping aktivieren</translation>
+        <translation>Tapping aktivieren</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="881"/>
         <source>Disable while typing</source>
-        <translation type="vanished">Während des Tippens deaktivieren</translation>
+        <translation>Während des Tippens deaktivieren</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="938"/>
         <source>Acceleration</source>
-        <translation type="vanished">Beschleunigung</translation>
+        <translation>Beschleunigung</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="954"/>
         <source>Accel profile</source>
-        <translation type="vanished">Profil</translation>
+        <translation>Profil</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="970"/>
         <source>Accel speed</source>
-        <translation type="vanished">Geschwindigkeit</translation>
+        <translation>Geschwindigkeit</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="745"/>
         <source>Button</source>
-        <translation type="vanished">Maustaste</translation>
+        <translation>Maustaste</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="746"/>
         <source>Edge</source>
-        <translation type="vanished">Rand</translation>
+        <translation>Rand</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="747"/>
         <source>Two-finger</source>
-        <translation type="vanished">Zweifinger</translation>
+        <translation>Zweifinger</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="748"/>
         <source>Mouse wheel</source>
-        <translation type="vanished">Mausrad</translation>
+        <translation>Mausrad</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="935"/>
         <source>Adaptive</source>
-        <translation type="vanished">Anpassend</translation>
+        <translation>Anpassend</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="936"/>
         <source>Flat</source>
-        <translation type="vanished">Konstant</translation>
+        <translation>Konstant</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="769"/>
         <source>Scroll method</source>
-        <translation type="vanished">Scroll-Methode</translation>
+        <translation>Scroll-Methode</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="788"/>
         <source>Scroll button</source>
-        <translation type="vanished">Scroll-Taste</translation>
+        <translation>Scroll-Taste</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="856"/>
         <source>Left, right, middle</source>
-        <translation type="vanished">Links, rechts, Mitte</translation>
+        <translation>Links, rechts, Mitte</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="857"/>
         <source>Left, middle, right</source>
-        <translation type="vanished">Links, Mitte, rechts</translation>
+        <translation>Links, Mitte, rechts</translation>
+    </message>
+    <message>
+        <location filename="../dsbxinput.in" line="753"/>
+        <source>Scrolling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dsbxinput.in" line="859"/>
+        <source>Tapping</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dsbxinput.in" line="872"/>
+        <source>Tapping drag lock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dsbxinput.in" line="900"/>
+        <source>Tapping button mapping</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
+        <location filename="../dsbxinput.in" line="1009"/>
         <source>&amp;Save</source>
-        <translation type="vanished">&amp;Speichern</translation>
+        <translation>&amp;Speichern</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="1010"/>
         <source>&amp;Quit</source>
-        <translation type="vanished">&amp;Beenden</translation>
+        <translation>&amp;Beenden</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="1021"/>
         <source>Saved</source>
-        <translation type="vanished">Gespeichert</translation>
+        <translation>Gespeichert</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="998"/>
         <source>Mouse and Touchpad Configuration</source>
-        <translation type="vanished">Maus- und Touchpad-Konfiguration</translation>
+        <translation>Maus- und Touchpad-Konfiguration</translation>
     </message>
 </context>
 <context>
     <name>__app</name>
     <message>
+        <location filename="../dsbxinput.in" line="1037"/>
         <source>Fatal error</source>
-        <translation type="vanished">Kritischer Fehler</translation>
+        <translation>Kritischer Fehler</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="499"/>
         <source>Failed to Popen(&quot;{}&quot;): {}</source>
-        <translation type="vanished">Fehler bei Popen(&quot;{}&quot;) aufgetreten:{}</translation>
+        <translation>Fehler bei Popen(&quot;{}&quot;) aufgetreten:{}</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="479"/>
         <source>Command &quot;{}&quot; returned {}</source>
-        <translation type="vanished">Befehl &quot;{}&quot; mit Rückgabewert {} beendet</translation>
+        <translation>Befehl &quot;{}&quot; mit Rückgabewert {} beendet</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="517"/>
         <source>Command &quot;{} list-props {}&quot; returned {}</source>
-        <translation type="vanished">Befehl &quot;{} list-props {}&quot; mit Fehlercode {} beendet</translation>
+        <translation>Befehl &quot;{} list-props {}&quot; mit Fehlercode {} beendet</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="531"/>
         <source>Couldn&apos;t create {}: {}</source>
-        <translation type="vanished">Konnte {} nicht erstellen: {}</translation>
+        <translation>Konnte {} nicht erstellen: {}</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="543"/>
         <source>Couldn&apos;t open {} for writing: {}</source>
-        <translation type="vanished">Konnte {} nicht zum Schreiben öffnen: {}</translation>
+        <translation>Konnte {} nicht zum Schreiben öffnen: {}</translation>
     </message>
     <message>
+        <location filename="../dsbxinput.in" line="1037"/>
         <source>xinput is not installed. Please install x11/xinput</source>
-        <translation type="vanished">xinput ist nicht installiert. Bitte installieren Sie x11/xinput</translation>
+        <translation>xinput ist nicht installiert. Bitte installieren Sie x11/xinput</translation>
+    </message>
+</context>
+<context>
+    <name>msgbox</name>
+    <message>
+        <location filename="../dsbxinput.in" line="609"/>
+        <source>&lt;b&gt;Please note&lt;/b&gt;&lt;br/&gt;&lt;br/&gt;&lt;em&gt;{}&lt;/em&gt; saves the current mouse/touchpad configuration in the executable shell script &lt;em&gt;{}&lt;/em&gt;. If your window manager or desktop environment does not support XDG autostart, make sure the script gets executed on session start by adding the line &lt;code&gt;{}&amp;&lt;/code&gt; to your &lt;em&gt;~/.xinitrc&lt;/em&gt;, &lt;em&gt;~/.xsession&lt;/em&gt;, or your window manager&apos;s autostart file.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dsbxinput.in" line="620"/>
+        <source>Do not show again</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dsbxinput.in" line="624"/>
+        <source>Please note</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
